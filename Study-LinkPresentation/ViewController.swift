8052bd739ccd5a1f2444bf529992e50b83879a72
Takayuki Sei <takayuki.sei@gree.net>
2020-07-30 19:45:26 +0900

    initial commit

---
+++ Study-LinkPresentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Study-LinkPresentation
+//
+//  Created by Takayuki Sei on 2020/07/30.
+//  Copyright © 2020 Takayuki Sei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
